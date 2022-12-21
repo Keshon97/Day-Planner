fca3d5b55c3923364e693eae4bf4b4fcb9c96bf6
Keshon Royster <k.royster512@gmail.com>
2022-12-21 15:22:39 -0600

    created a function to color code time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var displayTime = $('#currentDay');
 //function to get current time using moment(format Dec 21st, 2022, 2:10pm)
-function getTime () {
-    var currentTime = moment().format('MMM Do, YYYY, hh:mm a');
-    displayTime.html(currentTime);
+function getTime() {
+  var currentTime = moment().format('MMM Do, YYYY, hh:mm a');
+  displayTime.html(currentTime);
 };
 //fuction that saves the note in local storage whenever the svae button is cliecked
 $(document).ready(function () {
@@ -15,7 +15,35 @@ $(document).ready(function () {
   });
 });
 
-  // will load the saved notes from local storage by the hour
+function colorNotes() {
+  // get current number of hours
+  var currentHour = moment().hours();
+
+  // loop over time blocks
+  $('.time-block').each(function () {
+    var hour = parseInt($(this).attr('id').split('-')[1]);
+
+    // check if we've moved past this time
+    if (hour === currentHour) {
+      $(this).removeClass('past');
+      $(this).addClass('present');
+    }
+    else if (hour < currentHour) {
+      $(this).addClass('past');
+    } else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  });
+}
+
+colorNotes();
+
+// set up interval to check if current time needs to be updated
+setInterval(colorNotes, 5000);
+
+// will load the saved notes from local storage by the hour
 function getNotes() {
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
